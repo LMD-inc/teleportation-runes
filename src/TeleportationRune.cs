19c436cdb37dc23bbf71aa9d8f187180f913e94f
Denis <riskabeker@gmail.com>
2021-04-26 01:52:43 +0300

    - Initial commit of file structure required to launch mod

---
+++ src/TeleportationRune.cs
@@ -0,0 +1,30 @@
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+[assembly: ModInfo( "TeleportationRunes",
+	Description = "Mod that simplifies traveling in the world using gothic-like teleportation stones.",
+	Website     = "https://github.com/LMD-inc/teleportation-runes",
+	Version     = "0.1.0",
+	Authors     = new []{ "LMD-inc" } )]
+
+namespace TeleporatationRunes
+{
+	public class TeleporatationRunesMod : ModSystem
+	{
+		public override void Start(ICoreAPI api)
+		{
+			
+		}
+		
+		public override void StartClientSide(ICoreClientAPI api)
+		{
+			
+		}
+		
+		public override void StartServerSide(ICoreServerAPI api)
+		{
+			
+		}
+	}
+}
