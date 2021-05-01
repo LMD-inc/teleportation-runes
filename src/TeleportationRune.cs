347628242361f0a3b53c67ea531c45ab4c6c07ec
Denis <riskabeker@gmail.com>
2021-05-01 16:59:17 +0300

    Finished work on beacon materials

--- src/TeleportationRune.cs
+++ src/TeleportationRune.cs
@@ -1,16 +1,18 @@
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Server;
+using Vintagestory.GameContent;
+using Vintagestory.API.MathTools;
 
-[assembly: ModInfo( "TeleportationRunes",
-	Description = "Mod that simplifies traveling in the world using gothic-like teleportation stones.",
-	Website     = "https://github.com/LMD-inc/teleportation-runes",
-	Version     = "0.1.0",
-	Authors     = new []{ "LMD-inc" } )]
+[assembly: ModInfo("TeleportationRunes",
+    Description = "Mod that simplifies traveling in the world using gothic-like teleportation stones.",
+    Website = "https://github.com/LMD-inc/teleportation-runes",
+    Version = "0.1.0",
+    Authors = new[] { "LMD-inc" })]
 
 namespace TeleporatationRunes
 {
-	public class TeleporatationRunesMod : ModSystem
+    public class TeleporatationRunesMod : ModSystem
 	{
 		public override void Start(ICoreAPI api)
 		{
@@ -36,6 +38,11 @@ namespace TeleporatationRunes
 
 	public class Beacon : BlockEntity 
 	{
-
+		public override void Initialize(ICoreAPI api) {
+			base.Initialize(api);
+			BEBehaviorAnimatable ats = GetBehavior<BEBehaviorAnimatable>();
+			System.Diagnostics.Debug.WriteLine("World side: " + api.World.Side.ToString());
+			System.Diagnostics.Debug.WriteLine("Animator: " + (ats == null));
+		}
 	}
 }
