a890ae769999275785cf1353b1972cd673c9b1d0
Denis <riskabeker@gmail.com>
2021-05-01 19:21:59 +0300

    Added correct block description on hover, improved animation

--- src/beacon/BeBeacon.cs
+++ src/beacon/BeBeacon.cs
@@ -1,6 +1,9 @@
+using System.Text;
+
 using Vintagestory.API.Common;
 using Vintagestory.GameContent;
 using Vintagestory.API.MathTools;
+using Vintagestory.API.Config;
 
 namespace TeleporatationRunes
 {
@@ -10,14 +13,22 @@ namespace TeleporatationRunes
         {
             base.Initialize(api);
 
-			BEBehaviorAnimatable animUtil = GetBehavior<BEBehaviorAnimatable>();
+            BEBehaviorAnimatable animUtil = GetBehavior<BEBehaviorAnimatable>();
 
-			if (api.World.Side == EnumAppSide.Client)
-			{
-                System.Console.WriteLine("Rotate: " + Block.Shape.rotateY);
-				animUtil.animUtil.InitializeAnimator("lmd:rune-beacon", new Vec3f(0, Block.Shape.rotateY, 0));
-				animUtil.animUtil.StartAnimation(new AnimationMetaData() { Animation = "Idle", Code = "idle", Weight = 1});
-			}
+            if (api.World.Side == EnumAppSide.Client)
+            {
+                animUtil.animUtil.InitializeAnimator("lmd:rune-beacon", new Vec3f(0, Block.Shape.rotateY, 0));
+                animUtil.animUtil.StartAnimation(new AnimationMetaData() { Animation = "Idle", Code = "idle", Weight = 1 });
+            }
+        }
+
+        public override void GetBlockInfo(IPlayer forPlayer, StringBuilder dsc)
+        {
+            base.GetBlockInfo(forPlayer, dsc);
+            dsc.AppendLine(Lang.Get("game:blockmaterial-Stone") + ": "
+                + Lang.Get("game:block-rock-" + this.Block.Variant["stone"]));
+            dsc.AppendLine(Lang.Get("game:blockmaterial-Metal") + ": "
+                + Lang.Get("game:material-" + this.Block.Variant["metal"]));
         }
     }
 }
